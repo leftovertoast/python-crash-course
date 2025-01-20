458f9c135dcf5d571a98cb90e5ee044cdbfaf5b8
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-20 16:51:12 -0500

    completed 3-8 Seeing the World

---
+++ Chapter_03/3-8_seeingTheWorld.py
@@ -0,0 +1,15 @@
+# Exercise 3-8 Seeing the World
+# Learning Objective: Utilize sorted(), reverse(), and sort() methods on a list.
+
+placesToVisit = ['Zimbabwe', 'Georgia', 'Anarctica', 'Puerto Rico', 'Cuba']
+print(placesToVisit)
+print(sorted(placesToVisit))
+print(placesToVisit)
+placesToVisit.reverse()
+print(placesToVisit)
+placesToVisit.reverse()
+print(placesToVisit)
+placesToVisit.sort()
+print(placesToVisit)
+placesToVisit.sort(reverse=True)
+print(placesToVisit)
