15f015f7d0666307c43e1be67f5bb3ff458ba632
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-20 17:34:37 -0500

    completed 3-11 Intentional Error

---
+++ Chapter_03/3-11_intentionalError.py
@@ -0,0 +1,9 @@
+# Exercise 3-11 Inentional Error
+# Learning Objective: Create and fix an error based on indexing a list. 
+myList = ['Zero', 'One', 'Two']
+
+#produces error
+#print(myList[3]); 
+
+#corrected 
+print(myList[-1])
