0d329db75ce5684fd2e80ed067a2a67c9d4042c1
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-21 12:24:13 -0500

    relocate file to appropriate chapter folder

---
+++ Chapter_04/4-6_oddNumbers.py
@@ -0,0 +1,7 @@
+# Exercise 4-6 Odd Numbers
+# Learning Objective: Use third argument of range() to produce list of odd #'s between 1 and 20.
+
+oddNums = [value for value in range(1, 21, 2)]
+print(oddNums)
+
+#I'm sure this is probably a one-liner. Looking forward to that book. 
