29a858b366a847a24f7ed99e645832d728f7edd1
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-21 12:02:59 -0500

    Completed 4-5 Summing a Million

---
+++ Chapter_04/4-5_summingAMillion.py
@@ -0,0 +1,8 @@
+# Exercise 4-5 Summing a Million 
+# Learning Objective: Utitlize Min, Max and Sum methods on a list created with a for loop. 
+
+
+myListToAMillion = [value for value in range(1, 1_000_001)]
+print(min(myListToAMillion))
+print(max(myListToAMillion))
+print(sum(myListToAMillion))
