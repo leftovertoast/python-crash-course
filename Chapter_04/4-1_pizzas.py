23a41bbd1a6500284862afbc076a97c5b721ecfb
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-20 23:16:15 -0500

    Completed 4-1 Pizzas

---
+++ Chapter_04/4-1_pizzas.py
@@ -0,0 +1,9 @@
+# Exercise 4-1 Pizzas 
+# Learning Objective: Utilize a for loop with a list.
+
+favoritePizzas = ['Meat lovers', 'White with Spinach and Feta', 'Pepperoni']
+
+for pizza in favoritePizzas:
+	print(f"I like {pizza} pizza. \n")
+
+print("I really love pizza!")
