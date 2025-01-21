13be77f828c5f2173172dcb74eadd0be5c2c16d2
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-21 13:30:46 -0500

    Completed 4-10 Slices

---
+++ Chapter_04/4-10_slices.py
@@ -0,0 +1,13 @@
+# Excercise 4-10 Slices
+# Learning Objective: Slice list for First, Middle, and Last 3 elements of a list respectively. 
+
+numbers = [value for value in range(1, 21)]
+print(str(numbers) + "\n")
+
+print(f"The first three items in the list are: {numbers[0:3]}\n")
+print(f"Three items from the middle of the list are: {numbers[9:12]}\n")
+print(f"The last three items in the list are: {numbers[-3:]}")
+
+# 100% this can be done better. 
+
+
