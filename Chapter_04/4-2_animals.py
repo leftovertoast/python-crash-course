50876dd94bfb56db68e3a6cae7e9765477238074
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-20 23:21:41 -0500

    Completed 4-2 Animals

---
+++ Chapter_04/4-2_animals.py
@@ -0,0 +1,16 @@
+# Exercise 4-2 Animals 
+# Learning Objective: Same as 4-1, utilize for loop. 
+
+animals = ['dog', 'cat', 'ferret']
+
+'''
+for animal in animals: 
+	print(animal)
+'''
+
+#modified: 
+
+for animal in animals: 
+	print(f"A {animal} would make a great pet. \n")
+
+print("These animals all have 4 legs and make great pets!")
