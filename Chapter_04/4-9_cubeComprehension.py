0f9e39a6d21a28fe03a1320d9244b1104f03cf14
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-21 12:57:22 -0500

    Completed 4-9 Cube Comprehension

---
+++ Chapter_04/4-9_cubeComprehension.py
@@ -0,0 +1,7 @@
+# Exercise 4-9 Cube Comprehension 
+# Learning Objective: Use List Comprehension to create list of first 10 cubes. 
+
+cubes = [value**3 for value in range(1, 11)]
+print(cubes)
+
+# Unbenownst to me, I had done that already in 4-8. 
