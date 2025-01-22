3a4c4543601ef6a2bfbbab1556f9cfa85f7a4252
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-22 15:27:53 -0500

    Completed 5-3 Alien Colors

---
+++ Chapter_05/5-03_alien_colors.py
@@ -0,0 +1,16 @@
+# Exercise 5-3 Alien Colors
+# Learning Objective: Utilize an if statement to determine the color of an alien in a game. 
+
+
+#passes 
+alien_color = 'green'
+
+if (alien_color == 'green'):
+	print('Player just scored 5 points!')
+
+#fails 
+if (alien_color == 'yellow'):
+    print('Player just scored 10 points!')
+
+
+
