33fdbb7cf5bb7732b1d6aa3f3a69576387e22ab0
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-22 15:36:26 -0500

    Completed 5-4 Alien Colors #2

---
+++ Chapter_05/5-04_alien_colors2.py
@@ -0,0 +1,18 @@
+# Exercise 5-4 Alien Colors #2 
+# Learning Objective: Utilize an if/else statement 
+
+
+# Runs if block:
+alien_color = 'green'
+
+if (alien_color == 'green'):
+    print("\nPlayer just scored 5 points!")
+else: 
+	print("\nPlayer just scored 10 points!")
+
+# Runs else block:
+
+if (alien_color != 'green'):
+    print("\nPlayer just socred 10 points!")
+else:
+	print("\nPlayer just scored 5 points!")
