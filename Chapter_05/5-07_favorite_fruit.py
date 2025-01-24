f09c5f103cec3d3764ef89ded2a0ce0d62323fe7
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-01-24 18:03:20 -0500

    Completed 5-7 Favorite Fruit

---
+++ Chapter_05/5-07_favorite_fruit.py
@@ -0,0 +1,16 @@
+# Exercise 5-7 Favorite Fruit 
+# Learning Objective: Write five if statements based on a list of favorite fruits. 
+
+favorite_fruits = ['apples', 'pears', 'bananas']
+
+if 'bananas' in favorite_fruits:
+	print('You really like bananas!')
+if 'pears' in favorite_fruits:
+	print('You really like pears!')
+if 'apples' in favorite_fruits:
+	print('You really like apples!')
+if 'strawberries' in favorite_fruits:
+	print('You really like strawberries')
+if 'blueberries' in favorite_fruits:
+	print("You really like blueberries")
+	
